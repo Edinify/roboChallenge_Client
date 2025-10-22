8d39f6f2b896007fa3f3def9055e771c42f60f2b
Edinify <141638391+Edinify@users.noreply.github.com>
2025-10-22 18:09:09 +0400

    Update apiRoot.js

--- src/apiRoot/apiRoot.js
+++ src/apiRoot/apiRoot.js
@@ -1,3 +1,1 @@
-// export const apiRoot = "https://robochallenge-server-uw9i.onrender.com/api";
-
-export const apiRoot = "http://localhost:4000";
+export const apiRoot = "https://robochallenge-server-uw9i.onrender.com/api";
