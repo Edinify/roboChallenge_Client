96d38af9de87d2ebbb08af86e25d8f8541f06184
SamedovResul <samedovrasul7@gmail.com>
2025-10-23 14:49:59 +0400

    /

--- src/apiRoot/apiRoot.js
+++ src/apiRoot/apiRoot.js
@@ -1,3 +1,2 @@
-// export const apiRoot = "https://robochallenge-server-uw9i.onrender.com/api";
+export const apiRoot = "https://robochallenge-server-uw9i.onrender.com/api";
 
-export const apiRoot = "http://localhost:4000/api";
